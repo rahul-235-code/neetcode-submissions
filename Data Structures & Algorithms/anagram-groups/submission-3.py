c3074e507233a5a9f9e1a0fe7a1fda02e067ed4f
rahul-235-code <72337051+rahul-235-code@users.noreply.github.com>
2026-05-17 15:33:01 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+
+        for s in strs:
+            char_list = [0]*26
+            for c in s:
+                char_list[ord(c)- ord("a")] +=1
+            res[tuple(char_list)].append(s)
+
+        
+        return list(res.values())
